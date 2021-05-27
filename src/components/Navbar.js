fec5c3634b505c694a590fb6586a3e9e00348854
AndyBeable <andy.beable@gmail.com>
2021-05-27 16:15:32 +0100

    navbar flyout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,57 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
-import Dropdown from './Dropdown';
 
 function Navbar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
 
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <nav className='navbar'>
         <Link to='/' className='navbar-logo'>
-          Audiophile
+          audiophile
         </Link>
         <div className='menu-icon' onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/headphones'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Headphones
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/speakers'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Speakers
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/earphones'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Earphones
+            </Link>
+          </li>
+        </ul>
       </nav>
     </>
   );
