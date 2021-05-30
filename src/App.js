761827475cda6439308793b8c8c61b7b3135aef3
AndyBeable <andy.beable@gmail.com>
2021-05-30 14:42:31 +0100

    hero img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Hero />
     </Router>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+function Hero() {
+  return (
+    <section className='section'>
+      <div className='wrapper'>
+        <span className='tagline'>New product</span>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
