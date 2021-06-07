a06e0773037328a002cae00c4f417df1894aa7b3
AndyBeable <andy.beable@gmail.com>
2021-06-07 14:52:45 +0100

    speaker amend

--- src/components/ZX7SpeakerCard.js
+++ src/components/ZX7SpeakerCard.js
@@ -5,9 +5,9 @@ import Button from './shared/Button/Button';
 
 const ZX7SpeakerCard = () => {
   return (
-    <div className='speaker_container'>
-      <div className='speaker_highlight_copy'>
-        <h2>ZX9 Speaker</h2>
+    <div className='zx7_speaker_container'>
+      <div className='zx7_highlight_copy'>
+        <h2>ZX7 Speaker</h2>
         <Button />
       </div>
     </div>
