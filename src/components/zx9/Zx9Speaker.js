18fb976c595e280534a96560192032f7137e48c0
AndyBeable <andy.beable@gmail.com>
2021-06-07 16:22:08 +0100

    zx9 render

--- src/components/zx9/Zx9Speaker.js
+++ src/components/zx9/Zx9Speaker.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import './Zx9Speaker.scss';
+import ZX9 from '../../assets/home/desktop/image-speaker-zx9.png';
 
 const Zx9Speaker = () => {
-  return <h1>Hi there</h1>;
+  return (
+    <div className='zx9'>
+      <div className='zx9_content_container'>
+        <img src={ZX9} />
+        <h2 className='zx9_title'>ZX9 Speaker</h2>
+        <p>
+          Upgrade to premium speakers that are phenomenally built to deliver
+          truly remarkable sound
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Zx9Speaker;
